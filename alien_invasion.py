190c9acde6411573bdcd6392ab18c2bda520ea9c
Rafael Feyskhanov <raffeiswork@yandex.ru>
2024-02-01 13:56:09 +0300

    right after fullscreen

--- alien_invasion.py
+++ alien_invasion.py
@@ -10,6 +10,10 @@ class AlienInvasion:
         """нициализирует игру и создаёт игровые ресурсы."""
         pygame.init()
         self.settings = Settings()
+# полноэкранный режим
+#        self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+#        self.settings.screen_width = self.screen.get_rect().width
+#        self.settings.screen_heght = self.screen.get_rect().height
         self.screen = pygame.display.set_mode(
             (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
